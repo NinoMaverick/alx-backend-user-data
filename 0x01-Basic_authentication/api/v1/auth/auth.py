9bff2b8ea3726d188cb7a94874ec611f00b9bfd1
ninomaverick <etengyemmy@gmail.comgit config --global user.email etengyemmy@gmail.com>
2024-10-11 22:59:22 +0100

    auth2

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,7 @@ class Auth:
      """Authentication class.
     """
 
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Checks if a path requires authentication.
         """
         if path is not None and excluded_paths is not None:
@@ -26,14 +26,14 @@ class Auth:
                     return False
         return True
 
-    def authorization_header(self, request=None) -> str:
+def authorization_header(self, request=None) -> str:
         """Gets the authorization header field from the request.
         """
         if request is not None:
             return request.headers.get('Authorization', None)
         return None
 
-    def current_user(self, request=None) -> TypeVar('User'):
+def current_user(self, request=None) -> TypeVar('User'):
         """Gets the current user from the request.
         """
         return None
